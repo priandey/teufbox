0cef6e03c8bf5e8ecb9e90849b6842c3aa51ba55
priandey <riandeypierre@gmail.com>
2020-03-27 17:48:57 +0100

    ugly code, still testing

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
 import os
 
-import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
-from models import MusicProposition
+from models import ExternalMusic
 
 scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+
+def search_engine(keep_going, youtube, keywords):
+    while keep_going:
+
+        request = youtube.search().list(
+            part="snippet",
+            maxResults=10,
+            q=keywords
+        )
+        response = request.execute()
+        response_list = []
+        position = 0
+        for proposition in response['items']:
+            try:
+                prop = ExternalMusic(name=proposition['snippet']['title'],
+                                     channel=proposition['snippet']['channelTitle'],
+                                     id=proposition['id']['videoId'])
+                response_list.append(prop)
+                print(f'{position}    -    {prop})')
+                position += 1
+            except KeyError:
+                pass
+
+        choice = input("DL one [0 - 9] or continue search (type search) or exit (type exit) ? : ")
+
+        if choice.isnumeric():
+            index = int(choice)
+            try:
+                response_list[index].download()
+            except IndexError:
+                print("Le chiffre doit être entre 0 et 9")
+
+        elif choice == 'exit':
+            keep_going = False
+        else:
+            keywords = choice
 
 def main():
     # Disable OAuthlib's HTTPS verification when running locally.
@@ -15,32 +50,16 @@ def main():
 
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = "client_secret.json"
+    DEVELOPER_KEY = os.getenv("GOOGLE_KEY")
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
+    youtube = googleapiclient.discovery.build(api_service_name,
+                                              api_version,
+                                              developerKey=DEVELOPER_KEY)
 
     # Now that api is authenticated, start process
-    keep_going = "y"
-    while keep_going == "y":
-        keywords = input("Search for a video : ")
-        request = youtube.search().list(
-            part="snippet",
-            q=keywords
-        )
-        response = request.execute()
-        response_list = []
-        for proposition in response['items']:
-            prop = MusicProposition(name=proposition['snippet']['title'],
-                                    channel=proposition['snippet']['channelTitle'],
-                                    id=proposition['id']['videoId'])
-            print(prop)
-        keep_going = input("Continue (y/n) ?")
-
+    keep_going = True
+    keywords = input("Search for a video : ")
+    search_engine(keep_going, youtube, keywords)
 
 if __name__ == "__main__":
     main()
--- models.py
+++ models.py
@@ -1,8 +1,47 @@
-class MusicProposition():
+from __future__ import unicode_literals
+import os
+import youtube_dl
+
+class ExternalMusic():
     def __init__(self, name, channel, id):
         self.name = name
         self.channel = channel
         self.id = id
+        self.yt_url = "https://www.youtube.com/watch?v="
+
+        self.complete_url()
 
     def __repr__(self):
-        return f'{self.name}, from {self.channel}'
+        return f'{self.name}, from {self.channel} = {self.yt_url}'
+
+    def complete_url(self):
+        self.yt_url += self.id
+
+    def download(self):
+        class MyLogger(object):
+            def debug(self, msg):
+                pass
+
+            def warning(self, msg):
+                pass
+
+            def error(self, msg):
+                print(msg)
+
+        def my_hook(d):
+            if d['status'] == 'finished':
+                print('Done downloading, now converting ...')
+
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'outtmpl': f'music/{self.name}-{self.id}',
+            'logger': MyLogger(),
+            'progress_hooks': [my_hook],
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([self.yt_url])
